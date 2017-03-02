e5894741194f9a8499202d4209eac15cd405b79d
Alex.KinK <altaudiolabs@gmail.com>
2017-03-02 17:15:40 +0000

    Removed cat-name from yarn and npm

--- src/components/reactTasks/index.js
+++ src/components/reactTasks/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Menu from './menu'
-
+import Cell from './cell'
 
 export default () => {
   return (
     <div>
       <Menu />
+      <Cell />
 
     </div>
   )
