504a08cce13fb84ca215df2ee3d9bb346bcd6932
Alex.KinK <altaudiolabs@gmail.com>
2017-03-03 14:57:32 +0000

    Add basic comment box fucntionality

--- src/components/reactTasks/commentsBox.js
+++ src/components/reactTasks/commentsBox.js
@@ -1,25 +1,61 @@
 import React from 'react'
+import _ from 'lodash'
 
 class CommentsBox extends React.Component {
 
   constructor() {
     super()
-    this.state = { comments: [{ name: 'Dan Ellin', comment: 'This game is totes amazeballs' }] }
+
+    // Declare comments array and current comment and name
+    this.state = { comments: [
+      { name: 'Dan Ellin', comment: 'This game is totes amazeballs' },
+      { name: 'Gordon Miller', comment: 'I love grand designs me' },
+      { name: 'Dan Ellin', comment: 'Gordon what is ur fave epsiode?' },
+      { name: 'h8GrndDesgns', comment: 'Kevin is a idiot' }
+    ],
+      comment: '',
+      name: '' }
+  }
+
+  // When comment input field changes set comment in state
+  commentChange(input) {
+    this.setState({ comment: input.target.value })
+  }
+
+  // When name input changes set name in state
+  nameChange(input) {
+    this.setState({ name: input.target.value })
+  }
+
+  // When add comment button clicked add current name and comment to comments state, and clear both input fields
+  addComment() {
+    this.setState({ comments:
+    [{ name: this.state.name, comment: this.state.comment },
+      ...this.state.comments]
+    })
+
+    this.setState({ comment: '', name: '' })
   }
 
   render() {
     return (
       <div>
-        <p>Comments</p>
+        <h3>Comments</h3>
 
-        <p>Add Comment</p>
+        <ul>
+          {_.map(this.state.comments, (comment) => {
+            return <li>{`${comment.name} commented: ${comment.comment}`}</li>
+          })}
+        </ul>
+
+        <h3>Add Comment</h3>
         <p>Name:</p>
-        <input />
+        <input value={this.state.name} onChange={(value) => this.nameChange(value)} />
 
         <p>Comment:</p>
-        <input onChange={(value) => handleCommentChange(value)} />
+        <input value={this.state.comment} onChange={(value) => this.commentChange(value)} />
 
-        <button onClick={() => handleClick()}>Add Comment</button>
+        <button onClick={(value) => this.addComment(value)}>Add Comment</button>
 
       </div>
     )
